0035de8c948ab39c56d17fdb15dcf7191577423e
설연수 <seolnavy@naver.com>
2022-09-19 22:04:34 +0900

    #1 api 모듈 생성

---
+++ module-boot-api/src/test/java/com/kakaobank/blogsearch/BlogSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaobank.blogsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
